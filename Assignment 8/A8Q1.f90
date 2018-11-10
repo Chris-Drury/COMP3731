dbf5a8db5e317f3423ef31bc7bc8bba6c9d421fd
Chris Drury <chrswrdrury@gmail.com>
2018-11-10 11:18:54 -0330

    Updated A8Q1.f90
    updated to find GCD of the whole sequence, not of each adjacent pair

--- Assignment 8/A8Q1.f90
+++ Assignment 8/A8Q1.f90
@@ -1,12 +1,12 @@
 PROGRAM A8Q1
 !---
-!Determines the GCD of ints a and b given that a > b and a != 0. This program uses recusrion 
+!Determines the GCD of a sequence of ints a and b given that a > b and a != 0. This program uses recusrion 
 !---
 IMPLICIT NONE
 INTEGER, DIMENSION(:), ALLOCATABLE :: MandNs, results
 INTEGER :: a, b, n, check, i
 
-PRINT *, "This program determines the GCD of each two elements in a sequence, in order"
+PRINT *, "This program determines the GCD of a sequence through iteration."
 DO
   	PRINT *, "Enter the size of the integer sequence (0 to stop)"
     READ *, n
@@ -17,20 +17,22 @@ DO
 	ELSE
     	PRINT *, "Please enter the integer sequence:"
 		READ *, MandNs
-        PRINT *, "GCDs: "
+        PRINT *, 'GCDs: '
         DO i = 1,n-1
           	a = MandNs(i)
             b = MandNs(i + 1)
+            MandNs(i) = 0
             IF (a .eq. 0 .and. b .eq. 0) STOP
     		IF (a .eq. 0) THEN
-      			results(i) = b
+      			MandNs(i+1) = b
    			ELSE IF (b .eq. 0) THEN
-      			results(i) = a
+      			MandNs(i+1) = a
     		ELSE
-      			results(i) = GCD(a,b)
+      			MandNs(i+1) = GCD(a,b)
     		END IF
-            PRINT *, results(i)
+            PRINT *, MandNs
         END DO
+        PRINT *, "Final GCD: ", MandNs(n)
 	END IF
     DEALLOCATE(MandNs, results)
 END DO
